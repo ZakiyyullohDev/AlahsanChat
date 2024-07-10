8ca8e263886559560804a371b90aac038ad5a56e
ZakiyyullohDev <141619922+ZakiyyullohDev@users.noreply.github.com>
2024-07-10 12:47:18 +0500

    Html and Css Finished!

---
+++ js/app.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const emojis = [
+        "😀", "😁", "😂", "🤣", "😃", "😄", "😅", "😆", "😉", "😊", "😋", "😎", "😍", "😘", "😗", "😙", "😚", "🙂",
+        "🤗", "🤩", "🤔", "🤨", "😐", "😑", "😶", "🙄", "😏", "😣", "😥", "😮", "🤐", "😯", "😪", "😫", "😴", "😌", "😛",
+        "😜", "😝", "🤤", "😒", "😓", "😔", "😕", "🙃", "🤑", "😲", "☹️", "🙁", "😖", "😞", "😟", "😤", "😢", "😭", "😦",
+        "😧", "😨", "😩", "😬", "😰", "😱", "😳", "🤯", "😵", "😡", "😠", "🤬", "😷", "🤒", "🤕", "🤢", "🤮", "🤧", "😇",
+        "🥳", "🥺", "🥵", "🥶", "🥴", "🤠", "🥳", "🥸"
+    ];
+
+    const messageSenderInput = document.getElementById("messageSenderInput");
+    const showEmojisBtn = document.getElementById("showEmojisBtn");
+    const emojiContainer = document.getElementById("emojiContainer");
+
+    showEmojisBtn.addEventListener("click", (event) => {
+        event.stopPropagation();
+        emojiContainer.classList.toggle("display-none");
+        if (!emojiContainer.classList.contains("display-none")) {
+            emojiContainer.innerHTML = "";
+            emojis.forEach(emoji => {
+                const span = document.createElement("span");
+                span.textContent = emoji;
+                span.addEventListener("click", (event) => {
+                    event.stopPropagation();
+                    messageSenderInput.value += emoji;
+                    emojiContainer.classList.add("display-none");
+                });
+                emojiContainer.appendChild(span);
+            });
+        }
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!emojiContainer.classList.contains("display-none") && !emojiContainer.contains(event.target) && !showEmojisBtn.contains(event.target)) {
+            emojiContainer.classList.add('display-none');
+        }
+    });
+});
