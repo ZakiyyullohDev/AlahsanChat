3b15fd824b218bdeaf5642b575a558f1c8d03e3f
ZakiyyullohDev <141619922+ZakiyyullohDev@users.noreply.github.com>
2024-07-12 16:28:59 +0500

    fixe d bug

--- js/profile.js
+++ js/profile.js
@@ -2,13 +2,13 @@ const userImgUpload = document.getElementById('userImgUpload');
 const profileImg = document.getElementById('profileImg');
 
 const userName = document.getElementById('userName');
-const useremail = document.getElementById('userEmail');
+const userEmail = document.getElementById('userEmail');
 const userAtName = document.getElementById('userAtName');
 const editerBtnEmail = document.getElementById('editerBtnEmail');
 
 const editerBtnAt = document.getElementById('editerBtnAt');
-const editerBtn = document.getElementById('editerBtn');
 const editerBtnName = document.getElementById('editerBtnName');
+const editerBtnPassword = document.getElementById('editerBtnPassword');
 
 const imgSaveBtn = document.getElementById('imgSaveBtn');
 const logOutBtn = document.getElementById('logOutBtn');
@@ -25,11 +25,10 @@ imgSaveBtn.addEventListener('click', () => {
     try {
         if (profileImg.src) {
             localStorage.setItem("UserImgSource", profileImg.src);
-            alert("Rasm Saqlandi!")
-            return ''
+            alert("Rasm Saqlandi!");
+            return '';
         }
         alert("Xatolik Yuz Berdi. Iltimos Keyinroq Urinib Ko'ring!");
-        
     } catch (error) {
         console.error(error);
         alert("Xatolik Yuz Berdi. Iltimos Keyinroq Urinib Ko'ring!");
@@ -44,57 +43,51 @@ document.addEventListener('DOMContentLoaded', () => {
         profileImg.setAttribute('width', '200px');
         profileImg.setAttribute('height', '200px');
     }
+
+    if (localStorage.getItem("UserName")) {
+        userName.textContent = JSON.parse(localStorage.getItem("UserName"));
+    }
+
+    if (localStorage.getItem("UserEmail")) {
+        userEmail.textContent = JSON.parse(localStorage.getItem("UserEmail"));
+    }
+
+    if (!localStorage.getItem("UserName")) {
+        window.location.href = "login.html";
+    }
 });
 
-if (localStorage.key("UserName")) {
-    userName.textContent = JSON.parse(localStorage.getItem("UserName"))
-}
-
-if (localStorage.key("UserPassword")) {
-    userPassword.textContent = JSON.parse(localStorage.getItem("UserPassword"))
-}
-
-if (localStorage.key("UserEmail")) {
-    userEmail.textContent = JSON.parse(localStorage.getItem("UserEmail"))
-}
-
-editerBtnName.addEventListener('click', ()=> {
-    userName.contentEditable = true
-    userName.addEventListener('keydown', (e)=> {
+editerBtnName.addEventListener('click', () => {
+    userName.contentEditable = true;
+    userName.addEventListener('keydown', (e) => {
         if (e.key === "Enter") {
-            userName.contentEditable = false
-            localStorage.setItem("UserName", userName.textContent)
+            userName.contentEditable = false;
+            localStorage.setItem("UserName", JSON.stringify(userName.textContent));
         }
-    })
-})
+    });
+});
 
-editerBtnEmail.addEventListener('click', ()=> {
-    userEmail.contentEditable = true
-    userEmail.addEventListener('keydown', (e)=> {
+editerBtnEmail.addEventListener('click', () => {
+    userEmail.contentEditable = true;
+    userEmail.addEventListener('keydown', (e) => {
         if (e.key === "Enter") {
-            userEmail.contentEditable = false
-            localStorage.setItem("UserEmauserEmail", userEmail.textContent)
+            userEmail.contentEditable = false;
+            localStorage.setItem("UserEmail", JSON.stringify(userEmail.textContent));
         }
-    })
-})
+    });
+});
 
-editerBtnPassword.addEventListener('click', ()=> {
-    userName.contentEditable = true
-    userName.addEventListener('keydown', (e)=> {
+editerBtnPassword.addEventListener('click', () => {
+    userPassword.contentEditable = true;
+    userPassword.addEventListener('keydown', (e) => {
         if (e.key === "Enter") {
-            userName.contentEditable = false
-            localStorage.setItem("UserName", userName.textContent)
+            userPassword.contentEditable = false;
+            localStorage.setItem("UserPassword", JSON.stringify(userPassword.textContent));
         }
-    })
-})
+    });
+});
 
-document.addEventListener('DOMContentLoaded', ()=> {
-    if (!localStorage.key("UserName")) {
-        window.location.href = "login.html"
-    }
-})
-
-logOutBtn.addEventListener('click', ()=> {
-    localStorage.clear()
-    window.location.href = 'login.html'
-})
+logOutBtn.addEventListener('click', () => {
+    localStorage.clear();
+    window.location.href = 'login.html';
+});
